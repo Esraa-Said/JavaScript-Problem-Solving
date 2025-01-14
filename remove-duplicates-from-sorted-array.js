0a382c5270a8094490a136894594cd9ab6ab8450
Esraa Said <esraa195182@feng.bu.edu.eg>
2025-01-14 22:13:00 +0200

    remove duplicates from sorted array

---
+++ remove-duplicates-from-sorted-array.js
@@ -0,0 +1,17 @@
+/*
+https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
+*/
+
+var removeDuplicates = function (nums) {
+  if (nums.length === 0) return 0;
+  let k = 1;
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] != nums[i - 1]) {
+      nums[k] = nums[i];
+      k++;
+    }
+  }
+  return k;
+};
+
+console.log(removeDuplicates([1, 1, 2]));
